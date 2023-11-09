e45cb49829580229922a32d9551f43aeeb420f74
Niket Patil <pniket7@gmail.com>
2023-11-09 16:10:49 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -21,19 +21,29 @@ def main():
     # Create a Streamlit text input for user input with a unique key
     user_input = st.text_input("User:", key="user_input")
 
-    # Create a Streamlit button with a unique key
+    # Create a Streamlit button with a unique key to send a message
     if st.button("Send", key="send_button"):
         # Update the chat session with the user's input
         sessionAdvisor.chat(user_input=user_input, verbose=False)
 
-        # Get the chat history, which includes the chatbot's response
-        chat_history = sessionAdvisor.messages
+    # Get the chat history, which includes the chatbot's responses
+    chat_history = sessionAdvisor.messages
 
-        # Display the chat history
-        for message in chat_history:
-            role = message['role']
-            content = message['content']
-            st.markdown(f'**{role.capitalize()}:** {content}', unsafe_allow_html=True)
+    # Display the conversation history, including previous questions and answers
+    for message in chat_history:
+        role = message['role']
+        content = message['content']
+        st.markdown(f'**{role.capitalize()}:** {content}', unsafe_allow_html=True)
+
+    # Add a "New Chat" button to start a new conversation
+    if st.button("New Chat"):
+        # Clear the chat history to start a new conversation
+        sessionAdvisor.clear()
+
+    # Add an "Exit Chat" button to exit the current conversation
+    if st.button("Exit Chat"):
+        st.write("You have exited the current conversation.")
+        st.stop()  # This stops the Streamlit app
 
 if __name__ == "__main__":
     main()
